a2baa4201aa63dd51834e485a2f79dcd9f4c8081
Tlinmo <6a6o4ca@gmail.com>
2024-02-29 18:59:32 +0300

    Fix bugs

--- static/js/chat-web-socket.js
+++ static/js/chat-web-socket.js
@@ -31,6 +31,24 @@ function ofUrl(url) {
   }
   return o;
 }
+function getUsernameByUserId(id) {
+  jsonData = fetch('api/users/').json();
+  for (const user of jsonData) {
+    if (user.id === id) {
+      return user.username;
+    }
+  }
+  return null; // Если пользователь с таким айди не найден
+}
+function getTitleIdByChatId(id) {
+  jsonData = fetch('api/chats/').json();
+  for (const chat of jsonData) {
+    if (chat.title === usernaidme) {
+      return chat.title;
+    }
+  }
+  return null; // Если чат с таким айди не найден
+}
 
 // Обработчик события открытия соединения
 socket.onopen = function (event) {
@@ -54,11 +72,15 @@ socket.onmessage = function (event) {
   if (receivedData.type == "personal-message") {
     console.log('Получено личное сообщение');
 
-    const newText = receivedData.sender_username + ": " + receivedData.message;
+    const sender_id = receivedData.sender_id;
+    const sender_username = getUsernameByUserId(sender_id);
+    const message = receivedData.message;
+
+    const newText = sender_username + ": " + message;
     const newTextDiv = createDivWithText(newText); // Создаем div с текстом полученым из ответа
     const newNotification = newText;
 
-    if (receivedData.sender_id == urlParams.id) {
+    if (sender_id == urlParams.id) {
       appendMessage(newTextDiv)
     } else {
       showPopup(newNotification, 1000)
@@ -66,11 +88,17 @@ socket.onmessage = function (event) {
   } else if (receivedData.type == "group-message") {
     console.log('Получено групповое сообщение');
 
-    const newText = `${receivedData.sender_username}: ${receivedData.message}`;
-    const newTextDiv = createDivWithText(newText); // Создаем div с текстом полученым из ответа
-    const newNotification = `${receivedData.chat_title}(${receivedData.sender_username}): ${receivedData.message}`;
+    const sender_id = receivedData.sender_id;
+    const sender_username = getUsernameByUserId(sender_id);
+    const chat_id = receivedData.chat_id;
+    const chat_title = getTitleIdByChatId(chat_id);
+    const message = receivedData.message;
 
-    if (urlParams.type == "chat" && receivedData.chat_id == urlParams.id) {
+    const newText = `${sender_username}: ${message}`;
+    const newTextDiv = createDivWithText(newText); // Создаем div с текстом полученым из ответа
+    const newNotification = `${chat_title}(${sender_username}): ${message}`;
+
+    if (urlParams.type == "chat" && chat_id == urlParams.id) {
       appendMessage(newTextDiv)
     } else {
       showPopup(newNotification, 1000)
@@ -91,12 +119,26 @@ socket.onerror = function (error) {
 // Обработчик отправления сообщения(button or 'Enter')
 function sendMessage() {
   const message = document.getElementById('textInput').value;
-  const data = {
-    token: token,
-    reciver_user_id: urlParams.id,
-    reciver_username: userData.username,
-    message: message,
-  };
+
+  var data;
+
+  if (urlParams.type == "group") {
+    data = {
+      type: "group-message",
+      token: token,
+      sender_id: userData.id,
+      chat_id: urlParams.id,
+      message: message,
+    };
+  } else {
+    data = {
+      type: "personal-message",
+      token: token,
+      sender_id: userData.id,
+      reciver_id: urlParams.id,
+      message: message,
+    };
+  }
 
   fetch('/api/send-message/', {
     method: 'POST',
@@ -105,18 +147,17 @@ function sendMessage() {
     },
     body: JSON.stringify(data)
   })
-  .then(response => {
-    if (response.ok) {
-      document.getElementById('textInput').value = "";
-      return response.json();
-    }
-    throw new Error('Network response was not ok.');
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+    .then(response => {
+      if (response.ok) {
+        document.getElementById('textInput').value = "";
+        return response.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 }
-
